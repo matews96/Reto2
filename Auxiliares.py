9803e7d0729921c067ff45275b33569649800222
Mateo Echeverry <Mateo.X.Echeverry.-ND@disney.com>
2018-04-18 20:26:45 -0500

    mask

--- Auxiliares.py
+++ Auxiliares.py
@@ -34,3 +34,13 @@ def normalizeBGR(image):
     out.append(planes[2])
     out = cv2.merge(out)
     return out
+
+def getMask(img):
+
+    imgblurred = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.addWeighted(img, 1.5, imgblurred, -0.5, 0)
+    imHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    planes = cv2.split(imHSV)
+    ret, thresh2 = cv2.threshold(planes[1], 80, 255, cv2.THRESH_TRUNC)
+    ret, thresh2 = cv2.threshold(thresh2, 50, 255, cv2.THRESH_BINARY)
+    return thresh2
