dee2ca31771b53bcfc547a18d7f37c1b9df572d1
Mateo Echeverry <Mateo.X.Echeverry.-ND@disney.com>
2018-04-19 19:46:45 -0500

    ohj

--- Reto2.py
+++ Reto2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import Auxiliares as aux
 
-img = cv2.imread("Data/IMG_0173.JPG")
+img = cv2.imread("Data/IMG_0167.JPG")
 img = cv2.resize(img, None, fx=0.15, fy=0.15)
 mask = aux.getMask(img, True)
 cv2.imshow('img', img)
@@ -33,51 +33,45 @@ imgAB = cv2.merge(imgAB)
 img = imgAB
 
 
-img2 = img.reshape((img.shape[0] * img.shape[1],2)) #represent as row*column,channel number
-print(img.shape)
+ #represent as row*column,channel number
+
 axes = plt.gca()
 axes.set_xlim([0,250])
 axes.set_ylim([0,250])
 plt.show()
 
 
-clt = KMeans(n_clusters=7) #cluster number
-clt.fit(img2)
-clt.fit_predict(img2)
 
-labels = clt.labels_.reshape(img.shape[0], img.shape[1])
-planes[1][np.where(labels==0)] = clt.cluster_centers_[0][0]
-planes[1][np.where(labels==1)] = clt.cluster_centers_[1][0]
-planes[1][np.where(labels==2)] = clt.cluster_centers_[2][0]
-planes[1][np.where(labels==3)] = clt.cluster_centers_[3][0]
-planes[1][np.where(labels==4)] = clt.cluster_centers_[4][0]
-planes[1][np.where(labels==5)] = clt.cluster_centers_[5][0]
-planes[1][np.where(labels==6)] = clt.cluster_centers_[6][0]
+def getClusteredImage(img2):
+    a = img2.shape[0]
+    b = img2.shape[1]
+    img2 = img.reshape((img.shape[0] * img.shape[1], 2))
+    clt = KMeans(n_clusters=5)  # cluster number
+    clt.fit(img2)
+    clt.fit_predict(img2)
+
+    labels = clt.labels_.reshape(a, b)
+    print(len(set(clt.labels_)))
+
+    for i in range(len(set(clt.labels_))):
+        planes[1][np.where(labels == i)] = clt.cluster_centers_[i][0]
+        planes[2][np.where(labels == i)] = clt.cluster_centers_[i][1]
+
+    final = []
+    a = 100 * np.ones(planes[0].shape, dtype='uint8')
+    a[np.where(labels == 0)] = 500
+    final.append(a)
+    final.append(planes[1])
+    final.append(planes[2])
+    print(planes[0].shape)
+    print(a)
+    image = cv2.merge(final)
+    image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+
+    cv2.imshow('bar', image)
 
 
-planes[1][np.where(labels==0)] = clt.cluster_centers_[0][1]
-planes[1][np.where(labels==1)] = clt.cluster_centers_[1][1]
-planes[1][np.where(labels==2)] = clt.cluster_centers_[2][1]
-planes[1][np.where(labels==3)] = clt.cluster_centers_[3][1]
-planes[1][np.where(labels==4)] = clt.cluster_centers_[4][1]
-planes[1][np.where(labels==5)] = clt.cluster_centers_[5][1]
-planes[1][np.where(labels==6)] = clt.cluster_centers_[6][1]
-
-
-final = []
-final.append(planes[0])
-final.append(planes[1])
-final.append(planes[2])
-
-image = cv2.merge(final)
-image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
-
-#hist = find_histogram(clt)
-#bar = plot_colors2(hist, clt.cluster_centers_)
-#bar = cv2.cvtColor(bar, cv2.COLOR_LAB2BGR)
-
-cv2.imshow('bar', image)
-
+getClusteredImage(img)
 
 #plt.axis("off")
 #plt.imshow(bar)
